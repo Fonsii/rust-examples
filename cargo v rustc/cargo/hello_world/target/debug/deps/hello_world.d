1742770c4f73202693fb86381f27dca40e4f48a0
Fonsii <lualfonso14200174@gmail.com>
2022-10-24 13:06:56 -0600

    Cargo vs Rustc

---
+++ cargo v rustc/cargo/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\Luis\Desktop\TCU\rust-examples\cargo v rustc\cargo\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\Luis\Desktop\TCU\rust-examples\cargo v rustc\cargo\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ cargo v rustc/cargo/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\Luis\Desktop\TCU\rust-examples\cargo\ v\ rustc\cargo\hello_world\target\debug\hello_world.exe: C:\Users\Luis\Desktop\TCU\rust-examples\cargo\ v\ rustc\cargo\hello_world\src\main.rs

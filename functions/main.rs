1207946323544ca585d7a0ad1024a3011e6c3d30
Fonsii <lualfonso14200174@gmail.com>
2022-10-24 14:00:32 -0600

    Changes Functions

--- functions/main.rs
+++ functions/main.rs
@@ -3,9 +3,16 @@ fn say_hello(){
     println!("{}", hello);
 }
 
+fn return_function(letters: &str) -> String{
+    let mut salute = String::from("Salute ");
+    salute.push_str(letters);
+    salute
+}
+
 fn main(){
     let hello = "Hello World in Main";
     say_hello();
-
+    let salute = return_function(hello);
+    println!("{}", salute);
     println!("{}", hello);
 }

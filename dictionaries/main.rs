f60ee7214c00970251f292e0a2bcc1f37468af7e
Fonsii <lualfonso14200174@gmail.com>
2022-10-25 13:30:00 -0600

    Adding more examples to Dic

--- dictionaries/main.rs
+++ dictionaries/main.rs
@@ -1,5 +1,6 @@
 fn main(){
     use std::collections::HashMap;
+
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Albert"), 10);
@@ -8,6 +9,10 @@ fn main(){
     scores.insert(String::from("Maria"), 9);
 
     println!("{:?}", scores);
+    println!("{:?}", scores.get("Albert"));
+
+    scores.insert(String::from("Bob"), 6);
+
 
     if !scores.contains_key("Ana") {
         println!("Ana is not in the scores");

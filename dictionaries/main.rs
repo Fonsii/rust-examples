631f91aef2cf4598aab713a222ce74dafac08445
Fonsii <lualfonso14200174@gmail.com>
2022-10-25 11:01:20 -0600

    Dictionaries Example

---
+++ dictionaries/main.rs
@@ -0,0 +1,20 @@
+fn main(){
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Albert"), 10);
+    scores.insert(String::from("Bob"), 5);
+    scores.insert(String::from("Alexa"), 7);
+    scores.insert(String::from("Maria"), 9);
+
+    println!("{:?}", scores);
+
+    if !scores.contains_key("Ana") {
+        println!("Ana is not in the scores");
+    }
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+}
